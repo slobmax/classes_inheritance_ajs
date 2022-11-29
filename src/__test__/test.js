2bba76f52b5a1f4a47828d683f70da1808dbbd68
Maxim Slobozhanin <slob.max@gmail.com>
2022-11-30 02:58:57 +0400

    revision test.js

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -6,82 +6,88 @@ import Swordsman from '../swordsman';
 import Undead from '../undead';
 import Zombie from '../zombie';
 
-test('character bowman', () => {
-  const bowman = new Bowman('Bowman', 'Bowman');
-  expect(bowman).toEqual({
+test('Создание нового персонажа Bowman', () => {
+  const received = new Bowman('Bowman', 'Bowman');
+  const expected = {
     name: 'Bowman',
     type: 'Bowman',
     health: 100,
     level: 1,
     attack: 25,
     defence: 25,
-  });
+  };
+  expect(received).toEqual(expected);
 });
 
-test('character daemon', () => {
-  const daemon = new Daemon('Daemon', 'Daemon');
-  expect(daemon).toEqual({
+test('Создание нового персонажа Daemon', () => {
+  const received = new Daemon('Daemon', 'Daemon');
+  const expected = {
     name: 'Daemon',
     type: 'Daemon',
     health: 100,
     level: 1,
     attack: 10,
     defence: 40,
-  });
+  };
+  expect(received).toEqual(expected);
 });
 
-test('character magician', () => {
-  const magician = new Magician('Magician', 'Magician');
-  expect(magician).toEqual({
+test('Создание нового персонажа Magician', () => {
+  const received = new Magician('Magician', 'Magician');
+  const expected = {
     name: 'Magician',
     type: 'Magician',
     health: 100,
     level: 1,
     attack: 10,
     defence: 40,
-  });
+  };
+  expect(received).toEqual(expected);
 });
 
-test('character swordsman', () => {
-  const swordsman = new Swordsman('Swordsman', 'Swordsman');
-  expect(swordsman).toEqual({
+test('Создание нового персонажа Swordsman', () => {
+  const received = new Swordsman('Swordsman', 'Swordsman');
+  const expected = {
     name: 'Swordsman',
     type: 'Swordsman',
     health: 100,
     level: 1,
     attack: 40,
     defence: 10,
-  });
+  };
+  expect(received).toEqual(expected);
 });
 
-test('character undead', () => {
-  const undead = new Undead('Undead', 'Undead');
-  expect(undead).toEqual({
+test('Создание нового персонажа Undead', () => {
+  const received = new Undead('Undead', 'Undead');
+  const expected = {
     name: 'Undead',
     type: 'Undead',
     health: 100,
     level: 1,
     attack: 25,
     defence: 25,
-  });
+  };
+  expect(received).toEqual(expected);
 });
 
-test('character zombie', () => {
-  const zombie = new Zombie('Zombie', 'Zombie');
-  expect(zombie).toEqual({
+test('Создание нового персонажа Zombie', () => {
+  const received = new Zombie('Zombie', 'Zombie');
+  const expected = {
     name: 'Zombie',
     type: 'Zombie',
     health: 100,
     level: 1,
     attack: 40,
     defence: 10,
-  });
+  };
+  expect(received).toEqual(expected);
 });
 
-test('genError', () => {
+test('Класс Character, выброс ошибки', () => {
   function genErr() {
-    const genError = new Character('n', 'm');
-    return genError;
+    const received = new Character('n', 'm');
+    return received;
   }
   expect(genErr).toThrow();
 });
